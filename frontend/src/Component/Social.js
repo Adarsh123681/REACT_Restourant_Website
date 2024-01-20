1f855f3ea65a48d2ee9cff1f15b34addfbac8190
adarsh tripathi <85093448+Adarsh123681@users.noreply.github.com>
2024-01-21 04:27:50 +0530

    Update Social.js

--- frontend/src/Component/Social.js
+++ frontend/src/Component/Social.js
@@ -3,12 +3,12 @@ function Social() {
     return (
         <div>
             <div className="links">
-                <div className="insta">
+                <div className="face">
                 <a href = "https://www.facebook.com/profile.php?id=100055286242670">
                 <img src={`/image/facebook.png`} alt=" facebook"    />
                 </a>
                 </div>
-                <div className="face">
+                <div className="insta">
                 <img src={`/image/instagram.png`} alt="instaram"  />
                 </div>
 
